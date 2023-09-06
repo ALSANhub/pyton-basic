4db360f858bbf736e8c09ba851de35b7daaf395b
ALSAN <125503777+ALSANhub@users.noreply.github.com>
2023-09-06 14:30:31 -0300

    mor 3 basics

---
+++ FOR.py
@@ -0,0 +1,11 @@
+n = int(input('Qauntos numeros serao digitados? '))
+
+soma = 0
+
+for i in range(0, n):
+    x = int(input('digite um numero: '));
+    soma = soma + x;
+
+print("SOMA = ", soma);
+
+
